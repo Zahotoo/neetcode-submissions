e3e03a5d187659ec7334379868e868523d940504
Jiahao Zhu <146834485+Zahotoo@users.noreply.github.com>
2026-09-22 04:04:59 +0100

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int maj = nums.length / 2;
+
+        for (int i = 0; i < nums.length; i++) {
+            int count = map.getOrDefault(nums[i], 0) + 1;
+            map.put(nums[i], count);
+
+            if (count > maj) {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+}
